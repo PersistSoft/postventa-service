3f4a11f0565a5d984b3a3b8f494931780b0ad7d8
bautistaj <bautistaj20@outlook.com>
2022-05-15 11:02:52 -0500

    (feat) configre modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PostventaModule } from './postventa/postventa.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PostventaModule],
   controllers: [AppController],
   providers: [AppService],
 })
